929c51965ad9a25cf9682badffa922bbd3e6c8e9
li.jie <li.jie@sand.com.cn>
2018-06-03 13:06:00 +0800

    Aspect的引介增强 @DeclareParents

--- spring3.x-chapter7/src/main/java/com/baobaotao/aspectj/basic/EnableSellerAspect.java
+++ spring3.x-chapter7/src/main/java/com/baobaotao/aspectj/basic/EnableSellerAspect.java
@@ -1,14 +1,19 @@
 package com.baobaotao.aspectj.basic;
+
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.DeclareParents;
 
 import com.baobaotao.Seller;
 import com.baobaotao.SmartSeller;
 
-
 @Aspect
 public class EnableSellerAspect {
-   @DeclareParents(value="com.baobaotao.NaiveWaiter",
-		   defaultImpl=SmartSeller.class)
-   public  Seller seller;
+
+	/**
+	 * 1、为NaiveWaiter添加接口实现
+	 * 2、默认的接口实现类
+	 * 3、要实现的目标接口
+	 * **/
+	@DeclareParents(value = "com.baobaotao.NaiveWaiter", defaultImpl = SmartSeller.class)
+	public Seller seller;
 }
